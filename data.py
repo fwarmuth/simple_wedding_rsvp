4424ed87e48be02ff3d9663a8bb2a5638df9b1a6
fabiocremo <35569857+fabiocremo@users.noreply.github.com>
2021-03-16 08:29:54 +0100

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,4 @@
 ACTORS = [
-{"id":26073614,"name":"Roberto e Francesca","people": [{"people_name": "Roberto"},{"people_name": "Francesca"},{"people_name": "Alessandro"},{"people_name": "Giorgia"}]},
-{"id":26083114,"name":"Paolo e Claudia","people": [{"people_name": "Paolo"},{"people_name": "Claudia"},{"people_name": "Veronica"},{"people_name": "Anna"}]},
-{"id":26083115,"name":"Massimo e Ida","people": [{"people_name": "Massimo"},{"people_name": "Ida"},{"people_name": "Alice"}]},
-{"id":21332317,"name":"Pippo e Pippa","people": [{"people_name": "Pippo"},{"people_name": "Pippa"},{"people_name": "Pluto"}]}
+{"id":26073614,"name":"Pippo Pluto","people": [{"people_name": "Pippo"},{"people_name": "Pluto"},{"people_name": "Minnie"},{"people_name": "Paperino"}]},
+{"id":26083114,"name":"Person1","people": [{"people_name": "Person1"},{"people_name": "Person2"},{"people_name": "Person3"},{"people_name": "Person4"}]}
 ]
